bf918df9605fad70b08e0e2ae973d398e5e998c3
ma1261032026-alt <ma1261032026@unab.edu.sv>
2026-03-03 20:08:11 -0600

    Ejemplo de suma python

---
+++ Semana6/suma.py
@@ -0,0 +1,50 @@
+# es un comentario
+
+## para declarar una variable
+nombre = "Saira"
+print(nombre)
+
+edad = 18
+
+print(nombre, edad, " years old")  ## concatenar
+# binario = 0
+# binario = 1
+
+
+# string = letras
+# objeto = 012345
+
+# inmutabilidad :
+# pero siempre deben de coservar su tipo
+
+## direccion # 0x
+## tipo => numerico o string (booleano, objeto)
+## tipo numero => prepara a la memoria
+## para guardar un tipo de dato string exa.
+
+## textos largos vamos a utilizar las comillas triples
+texto_largo = """"
+Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit.
+Sed do eiusmod tempor 
+incididunt ut labore et 
+dolore magna aliqua."""
+print(texto_largo)
+## Las comillas triples se usan para textos largos
+## mantiene el formato de lo que se copia entre ellas
+## al momento de compilarlas
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 + numero2
+print("El resultado de la suma es:", resultado)
+
+resultado = numero1 - numero2
+print("El resultado de la resta es:", resultado)
+
+resultado = numero1 / numero2
+print("El resultado de la división es:", resultado)
+
+resultado = numero1 * numero2
+print("El resultado de la multiplicación es:", resultado)

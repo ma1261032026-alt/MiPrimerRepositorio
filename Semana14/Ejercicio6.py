1ff45e73ea299d7c74ae59548b4b3ef3be1dbf68
ma1261032026-alt <ma1261032026@unab.edu.sv>
2026-05-07 16:45:56 -0600

    Ejercicio 6: Contar numeros aleatorios mayoes a 50

---
+++ Semana14/Ejercicio6.py
@@ -0,0 +1,14 @@
+import random
+
+
+def contar_mayores_a_50(lista):
+    contador = 0
+    for n in lista:
+        if n > 50:
+            contador += 1
+    return contador
+
+
+aleatorios = [random.randint(1, 100) for _ in range(10)]
+print(f"Lista generada: {aleatorios}")
+print(f"Mayores a 50: {contar_mayores_a_50(aleatorios)}")
